0e90c3ae1cdad2e198e11e86a33e7437feb262e6
wllrx <wllrx93@163.com>
2019-07-02 17:43:41 +0800

    zuul限流

---
+++ zuul-ratelimit/src/main/java/com/zoe/ZuulRatelimitApplication.java
@@ -0,0 +1,14 @@
+package com.zoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ZuulRatelimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulRatelimitApplication.class, args);
+    }
+
+}
